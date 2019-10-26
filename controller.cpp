a4d71b77cbdc84d75debf29722b200557f307f6b
Illya Shevchenko <scorpy@yandex-team.ru>
2019-10-26 16:34:54 +0300

    mvp controller

---
+++ controller.cpp
@@ -0,0 +1,44 @@
+#include "controller.h"
+#include "game.h"
+
+#include "painter.h"
+
+#include <iostream>
+
+void Controller::Run() {
+    window_ = new sf::RenderWindow(sf::VideoMode(1000, 1000), "SYLA");
+    Painter* painter = new Painter(window_);
+    game_ = new Game(painter);
+
+    double t = clock();
+    while (window_->isOpen()) {
+        sf::Event event;
+        while (window_->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) {
+                window_->close();
+                break;
+            }
+
+            if (event.type == sf::Event::KeyPressed) {
+                ProcessKey(event.key.code);
+            }
+        }
+        clock_t dt = clock() - t;
+        t = clock();
+        game_->Tick(dt * 1.0 / CLOCKS_PER_SEC);
+        window_->clear({76, 180, 115});
+        game_->Draw();
+        window_->display();
+    }
+}
+
+void Controller::ProcessKey(sf::Keyboard::Key key) {
+    if (key == sf::Keyboard::Escape) {
+        window_->close();
+        exit(0);
+    }
+    else {
+        game_->ProcessKey(key);
+    }
+}
---
+++ view.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "painter.h"
+
+const double size = 0.05;
+
+class View {
+private:
+    double x_;
+    double y_;
+public:
+    View(double x, double y);
+    void Move(double dx, double dy);
+    void Draw(Painter* painter);
+}
